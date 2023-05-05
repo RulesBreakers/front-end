ef73342f0572813311ac5057ea6ef350184f34fb
amourRamanantsiresy <hei.amour.2@gmail.com>
2023-05-06 00:56:53 +0300

    chore: add axios

---
+++ src/pages/login/style.ts
@@ -0,0 +1,19 @@
+import { Sx } from '../../common/types';
+
+export const container: Sx = {
+  width: '100vw',
+  height: '100vh',
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  background: '#CCFFF499',
+};
+
+export const login_card: Sx = {
+  width: '60vw',
+  height: '90vh',
+};
