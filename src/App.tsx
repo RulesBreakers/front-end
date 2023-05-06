3a163996d9fddca3ea7bd8d689b6725909ceff20
Ylan <hei.nalisoa@gmail.com>
2023-05-06 18:38:03 +0300

    feat: historie of dreams there

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,16 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Login } from './pages/login';
 import ChatBox from "./pages/chat-box/Chatterbox";
 import Chatterbox from "./pages/chat-box/Chatterbox";
+import Footer from "./pages/footer/footer";
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
         <Routes>
-          <Route path='/login' element={<Login />} />
-          <Route path='/dashboard' element={<Chatterbox />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/dashboard' element={<Chatterbox />} />
+            <Route path='/footer' element={<Footer />} />
         </Routes>
       </BrowserRouter>
       <RootComponents />
