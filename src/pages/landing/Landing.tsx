72bda52bf70fe7dd1197f063bc00857249bb6214
Ny Hasina VAGNO <hei.nyhasina@gmail.com>
2023-05-07 10:21:21 +0300

    feat: testimonials

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -2,9 +2,11 @@ import { Box } from '@mui/material';
 import { landingContainer, landingLayout } from './style';
 import { Hero } from '.';
 import { CustomMenu } from '../customMenu/customMenu';
+import { Carousel } from 'react-responsive-carousel';
 import { Footer } from '../customMenu/footer';
 import { WhatIsOnyrix } from '.';
 import Tarifs from './Tarifs';
+import "./carousel.css"
 
 export const Landing = () => {
   return (
@@ -14,6 +16,33 @@ export const Landing = () => {
         <Hero />
         <WhatIsOnyrix />
         <Tarifs />
+          <div className="testimonial">
+          <Carousel
+            showArrows
+            infiniteLoop
+            showThumbs={false}
+            showStatus={false}
+            autoPlay
+            interval={2000}
+            className="carousel"
+          >
+            {['1', '2'].map((elt: any, k: number) => (
+              <>
+                <div>
+                  <div className="carousel__item">
+                  <div className="carousel__item__image">
+                      <p className="testimonial__text">
+                      </p>
+                    </div>
+                    <p className="testimonial__text">
+                      
+                    </p>
+                  </div>
+                </div>
+              </>
+            ))}
+          </Carousel>
+        </div>
       </Box>
       <Footer />
     </Box>
